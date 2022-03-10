f4a171a96fa1935732f4deb13a8975059da989e6
Valeriia <bartfeld.ler@yandex.ru>
2022-03-10 18:51:18 +0200

    Task_23 done

---
+++ Task_23/Program.cs
@@ -0,0 +1,12 @@
+﻿// принимает число N и выдаёт таблицу кубов чисел от 1 до N
+Console.WriteLine("Введите целое положительное число");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Ниже приведена таблица квадратов от 1 до {N}");
+for (int i = 1; i < N+1; i++)
+{
+    Console.WriteLine($"{i}*{i}*{i} = {i*i*i}");
+    if (i % 5 == 0)
+    {
+    Console.WriteLine();
+    }
+}
